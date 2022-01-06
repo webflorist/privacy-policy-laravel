a97d59ab8ec0b602de283db400fd8a2493d043e9
Gerald Buttinger <gerald@code.florist>
2022-01-06 23:03:45 +0100

    FIX html code in cookie details

--- src/resources/views/cookie-details.blade.php
+++ src/resources/views/cookie-details.blade.php
@@ -2,11 +2,11 @@
     <h4>{{ $cookie['name'] }}</h4>
 
     <dl>
-        <dt>{{ \PrivacyPolicy::translate('cookies.purpose.title') }}</dt>
-        <dd>{{ \PrivacyPolicy::translate('cookies.purpose.'.$cookie['purpose']) }}</dd>
+        <dt>{!! \PrivacyPolicy::translate('cookies.purpose.title') !!}</dt>
+        <dd>{!! \PrivacyPolicy::translate('cookies.purpose.'.$cookie['purpose']) !!}</dd>
 
         @if($type === 'third_party')
-            <dt>{{ \PrivacyPolicy::translate('cookies.origin') }}</dt>
+            <dt>{!! \PrivacyPolicy::translate('cookies.origin') !!}</dt>
             <dd>
                 {{ $cookie['service'] }}
                 (<a href="#processor-{{$cookie['processor']}}">
@@ -15,10 +15,10 @@
             </dd>
         @endif
 
-        <dt>{{ \PrivacyPolicy::translate('cookies.written_on.title') }}</dt>
-        <dd>{{ \PrivacyPolicy::translate('cookies.written_on.'.$cookie['written_on']) }}</dd>
+        <dt>{!! \PrivacyPolicy::translate('cookies.written_on.title') !!}</dt>
+        <dd>{!! \PrivacyPolicy::translate('cookies.written_on.'.$cookie['written_on']) !!}</dd>
 
-        <dt>{{ \PrivacyPolicy::translate('cookies.duration.title') }}</dt>
-        <dd>{{ \PrivacyPolicy::translate('cookies.duration.'.$cookie['duration']) }}</dd>
+        <dt>{!! \PrivacyPolicy::translate('cookies.duration.title') !!}</dt>
+        <dd>{!! \PrivacyPolicy::translate('cookies.duration.'.$cookie['duration']) !!}</dd>
     </dl>
 </section>
